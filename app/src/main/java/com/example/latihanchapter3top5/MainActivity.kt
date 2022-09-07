e98cdd0e321926e9c3baa83c7f5140fa2cea76d2
farelibrahim34 <farrelibrahim34@gmail.com>
2022-09-07 22:34:59 +0700

    update

--- app/src/main/java/com/example/latihanchapter3top5/MainActivity.kt
+++ app/src/main/java/com/example/latihanchapter3top5/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
 
         val listProvinsi = arrayListOf(
             ListProvinsi("Papua Selatan", "Kabupaten Merauke", R.drawable.papua_selatan),
-            ListProvinsi("Papua Tengah", "Kabupaten Nabire", R.drawable.papua_selatan),
+            ListProvinsi("Papua Tengah", "Kabupaten Nabire", R.drawable.papuateng_karapao),
             ListProvinsi("Nusa Tenggara Timur", "Kupang", R.drawable.ntt),
             ListProvinsi("Sumatra Selatan", "Palembang", R.drawable.sumsel_limas),
             ListProvinsi("Sumatra Barat", "Padang", R.drawable.sumbar_gadang),
@@ -23,6 +23,21 @@ class MainActivity : AppCompatActivity() {
             ListProvinsi("Sulawesi Barat", "Mamuju", R.drawable.sulbar_boyang),
             ListProvinsi("Nanggroe Aceh Darussalam", "Aceh", R.drawable.nad_aceh),
             ListProvinsi(" Sumatra Utara", "Medan", R.drawable.sumut_bolon),
+            ListProvinsi("Riau","Pekanbaru", R.drawable.riau_selaso),
+            ListProvinsi("Papua","Jayapura", R.drawable.papua_honai),
+            ListProvinsi("Papua Barat","Manokwari", R.drawable.pabar_modaki),
+            ListProvinsi("Nusa Tenggara Barat","Mataram", R.drawable.ntb_loka),
+            ListProvinsi("Maluku Utara","Sofifi", R.drawable.malut_sasadu),
+            ListProvinsi("Maluku","Ambon", R.drawable.maluku_baileo),
+            ListProvinsi("Lampung","Bandar Lampung", R.drawable.lampung_nuwo),
+            ListProvinsi("Kepulauan Riau","Tanjung Pinang", R.drawable.kepri_atap),
+            ListProvinsi("Kalimantan Utara","Tanjung Selor", R.drawable.kalut_baloy),
+            ListProvinsi("Kalimantan Timur","Samarinda", R.drawable.kaltim_lamin),
+            ListProvinsi("Kalimantan Tengah","Palangkaraya", R.drawable.kalteng_betang),
+            ListProvinsi("Kalimantan Selatan","Banjar Baru", R.drawable.kalsel_bubungan),
+            ListProvinsi("Kalimantan Barat","Pontianak", R.drawable.kalbar_panjang),
+
+
 
         )
         var adapterProvinsi =DataAdapter(listProvinsi)
